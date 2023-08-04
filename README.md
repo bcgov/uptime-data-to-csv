28cf26e86b0f0acb336ee1d264bdfcc2fddef053
jTendeck <justin.tendeck@gov.bc.ca>
2023-08-04 13:50:45 -0700

    finish up first iteration

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# uptime-data-to-csv
+# Uptime.com Data to CSV
+
+## Purpose
+
+Fetches data from Uptime.com and outputs to a .csv file.
+
+## Uptime Check Primary Keys
+
+This script requires you to supply primary keys of the checks you would like to retrieve data for. In order to view primary keys:
+- Go to one of the Uptime status pages ([prod](https://uptime.com/statuspage/bcgov-dss), or [non-prod](https://uptime.com/statuspage/bcgov-dss-nonprod)), select a check, and copy the primary key from the end of the URL.
+- From the Uptime dashboard, select a check, and copy primary key from end of the URL.
+
+> **Info**
+> You can supply multiple primary keys by separating keys by a comma. Eg: `-pk 13896,16934`
+
+## Usage
+
+To view available options, run:
+
+```sh
+python3 script.py -h
+```
+
+You must first save an Uptime API token as an environment variable:
+
+```sh
+export TOK="" # Uptime API Token
+```
+
+Then run:
+
+```sh
+# Get check data from today's date, going back in time by the default amount
+python3 script.py -t $TOK -pk 1389736 -d $(date +"%Y-%m-%d")
+```
+
+### Other Samples
+
+```sh
+# Get check data for the check with pk 1389736
+python3 script.py -t $TOK -pk 1389736
+
+# Get check data for check starting on 2023-07-23 (note: this check may fail if running in the future.)
+python3 script.py -t $TOK -pk 1389736 -d "2023-07-23"
+
+# Get check data from today's date, fetching data from 10 days back:
+python3 script.py -t $TOK -pk 1389736 -n 10
+
+# Get check data from multiple checks:
+python3 script.py -t $TOK -pk 1389736,1662934
+
+# Output a csv for alert data only:
+python3 script.py -t $TOK -pk 1389736 -o alerts
+```
--- script.py
+++ script.py
@@ -1,15 +1,21 @@
 import argparse
 import datetime as dt
-import json
-from datetime import timedelta
-
+from datetime import timedelta, datetime
+import csv
+import re
+import time
 import requests
 
 CONFIG = {
     'api': 'https://uptime.com/api/v1/',
     'token': '',
-    'headers': {},
+    'headers': {}
 }
+
+CHECK_NAMES = {}
+EST_CALLS = 0
+UPTIME_MAX_RPM = 60
+DEFAULT_DAYS = 30
 
 
 def parse_date(d):
@@ -17,55 +23,135 @@ def parse_date(d):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Download stats in bulk for all checks.')
-    parser.add_argument('--token', required=True,
+    parser = argparse.ArgumentParser(
+        description='Download stats in bulk for all checks.')
+    parser.add_argument('-t', '--token', required=True,
                         help='Your Uptime.com API Token')
+    parser.add_argument('-pk', '--check_pk', required=True,
+                        help='Primary key of the checks you would like to pull data from. If supplying multiple keys, separate with a comma. Eg: 147,789')
+    parser.add_argument('-d', '--date', type=parse_date,
+                        help='(optional) Date to end saving statistics from, YYYY-MM-DD. If not supplied, will use current date.')
+    parser.add_argument('-n', '--num_days',
+                        help='(optional) Number of days to fetch data from "--date". Default is %s.' % DEFAULT_DAYS)
+    parser.add_argument('-o', '--output',
+                        help='(optional) Specify type of data to output (response_time, alerts, etc.)')
     parser.add_argument('--api',
                         help='(optional) The Uptime.com API endpoint to use, eg. '
                              'https://uptime.com/api/v1/')
-    parser.add_argument('-d', '--date', required=True, type=parse_date,
-                        help='Date to end saving statistics from, YYYY-MM-DD')
 
     return parser.parse_args()
 
 
-def make_api_call(label, method, endpoint, pk=None, params=None, json=None):
+def make_api_call(label, method, endpoint, pk=None, params=None, json=None, slow_down=False):
     """Make an Uptime.com API call with the given method, endpoint and parameters."""
     url = CONFIG['api'] + endpoint.format(pk=pk)
 
-    r = requests.request(method, url, params=params, json=json, headers=CONFIG['headers'])
+    if slow_down:
+        time.sleep(1.05)
+
+    r = requests.request(method, url, params=params,
+                         json=json, headers=CONFIG['headers'])
     r.raise_for_status()
     return r.json()
 
 
-def load_all_checks_stats(from_date):
+def load_all_checks_stats(from_date, check_pk, days):
     """Load info and stats for all checks in the account, spanning multiple pages
     of results if necessary."""
-    current_date = from_date - timedelta(days=3) #CHANGE TO 30 DAYS, SET TO 3 FOR QUICKER TESTING
-    termination_date = from_date + timedelta(days=1)
+    global EST_CALLS
+
     stats = []
 
-    while current_date != termination_date:
-        r = make_api_call('Reading check stats',
-                          'get', 'checks/bulk/stats/',
-                          params={'pk': '1389736',
-                                  'start_date': str(current_date),
-                                  'end_date': str(current_date),
-                                  'include_alerts': '1'})
-        stats.append(r['checks'])
-        current_date = current_date + timedelta(days=1)
+    check_pks = check_pk.split(',')
+    days = DEFAULT_DAYS if not days else int(days)
+    # If no pk specified, will return data for all checks. Will want to apply request slow down.
+    EST_CALLS = "> 60" if not check_pk else len(
+        check_pks) + len(check_pks) * (days + 1)
+
+    print("Est. number of calls: %s" % EST_CALLS)
+    slow_down = True if not check_pk else EST_CALLS > UPTIME_MAX_RPM
+    if slow_down:
+        print("Est. number of calls exceeds Uptime rate limit of %s req/min. Request slow down will occur." % UPTIME_MAX_RPM)
+    else:
+        print("Request slowdown will NOT occur.")
+
+    for cpk in check_pk.split(','):
+        print("Fetching data for check with primary key: %s ..." % cpk)
+        current_date = from_date - \
+            timedelta(
+                days=days)  # CHANGE TO 30 DAYS, SET TO 3 FOR QUICKER TESTING
+        termination_date = from_date + timedelta(days=1)
+        r = make_api_call("Get check name", 'get', 'checks/%s' %
+                          cpk, slow_down=slow_down)
+        CHECK_NAMES[cpk] = r['name']
+
+        while current_date != termination_date:
+            r = make_api_call('Reading check stats',
+                              'get', 'checks/bulk/stats/',
+                              params={'pk': cpk,
+                                      'start_date': str(current_date),
+                                      'end_date': str(current_date),
+                                      'include_alerts': '1'}, slow_down=slow_down)
+            stats.append(r['checks'])
+            current_date = current_date + timedelta(days=1)
 
     return stats
 
 
+def output_res_time_csv(stats):
+    csv_file = "response_time.csv"
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(["checkPK", "checkName", "dateTime", "responseTime"])
+
+        for stat in stats:
+            for entry in stat:
+                pk = entry["pk"]
+                check_name = CHECK_NAMES.get(str(pk), '')
+                for stats in entry["statistics"]:
+                    for datapoint in stats["response_time_datapoints"]:
+                        dateTime, responseTime = datapoint
+                        writer.writerow(
+                            [pk, check_name, dateTime, responseTime])
+
+
+def output_alerts_csv(stats):
+    csv_file = "alerts.csv"
+    with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(["alertPK", "checkPK", "checkName", "createdAt",
+                        "endedAt" "stateIsUp", "ignored", "durationSecs", "output"])
+
+        for stat in stats:
+            for entry in stat:
+                check_pk = entry["pk"]
+                check_name = CHECK_NAMES.get(str(check_pk), '')
+                for stats in entry["statistics"]:
+                    for alert in stats["alerts"]:
+                        writer.writerow([alert["pk"], check_pk, check_name, alert["created_at"], alert["ended_at"],
+                                        alert["state_is_up"], alert["ignored"], alert["duration_secs"], re.sub(r'\s+', ' ', alert["output"])])
+
+
 def main():
     """Program entry point."""
+
+    print("WARNING: Uptime API has a rate limit of 60 calls per minute. If more than 60 calls are required, a delay will be placed in an attempt to comply with this limit.")
+
     opts = parse_args()
     CONFIG['api'] = opts.api or CONFIG['api']
     CONFIG['headers'] = {'Authorization': 'token ' + opts.token}
 
-    stats = load_all_checks_stats(opts.date)
-    print(json.dumps(stats, indent=4))
+    date = opts.date if opts.date else datetime.today()
+    stats = load_all_checks_stats(date, opts.check_pk, opts.num_days)
+
+    output_types = [] if not opts.output else opts.output.split(',')
+
+    if not opts.output or 'response_time' in output_types:
+        output_res_time_csv(stats)
+    if not opts.output or 'alerts' in output_types:
+        output_alerts_csv(stats)
 
 
 if __name__ == '__main__':
